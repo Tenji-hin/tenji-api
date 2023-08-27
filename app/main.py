822c33c23ba3c95dd86f7d16a06023a7b90a0582
Nate Shoffner <nate.shoffner@gmail.com>
2023-08-27 11:15:05 -0400

    Logtail

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
+from distutils.util import strtobool
+import logging
+import os
 from fastapi import FastAPI
 import aioredis
-
-
+from dotenv import load_dotenv
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
-
 from app.api.routes import mfc
+from logtail import LogtailHandler
+
+load_dotenv(".env")
+DEBUG = bool(strtobool(os.getenv("DEBUG", "True")))
+
+logger = logging.getLogger("TenjiLogger")
+logging.root.setLevel(logging.INFO if not DEBUG else logging.DEBUG)
+
+if os.getenv("LOGTAIL_TOKEN"):
+    logger.addHandler(LogtailHandler(os.getenv("LOGTAIL_TOKEN")))
+
+logger.info("Starting up...")
 
 app = FastAPI(
     title="MyFigureCollection API",
@@ -14,7 +27,6 @@ app = FastAPI(
 )
 
 app.include_router(mfc.router)
-
 
 @app.on_event("startup")
 async def startup():
