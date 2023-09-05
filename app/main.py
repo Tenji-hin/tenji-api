32c60ed920a429f7ce3b6256238a18929694ec8f
Nate Shoffner <nate.shoffner@gmail.com>
2023-09-05 04:03:11 -0400

    CORS

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 import aioredis
 from dotenv import load_dotenv
 from fastapi_cache import FastAPICache
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache.backends.redis import RedisBackend
 from app.api.routes import items, lists, shops, users
 from logtail import LogtailHandler
@@ -24,6 +25,14 @@ app = FastAPI(
     title="MyFigureCollection API",
     description="An unofficial API for MyFigureCollection.net",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(items.router)
